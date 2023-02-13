b58d935c4f9130929c446a670d9655b391af08a7
Azeh0 <ninjamaster1551@gmail.com>
2023-02-13 18:55:25 -0500

    Created Array.java file

---
+++ Array.java
@@ -0,0 +1,71 @@
+
+import java.util.Scanner;
+
+public class Array {
+  
+  
+  public static void main(String[] args) { 
+    
+    Scanner Keyboard = new Scanner(System.in);
+   /* #1a. Input 10 number, output the numbers */
+    int cnt;
+    
+    int [] MyArray = new int[10];
+    
+    for(cnt = 0; cnt < 10; cnt++)
+    {
+      
+    MyArray[cnt] = Keyboard.nextInt();
+    
+    }
+   /* b. output the values in reverse order. */
+      
+    for(cnt = 10 - 1; cnt >=0; cnt--)
+    {
+      
+      System.out.println(MyArray[cnt]);
+      
+    }
+    
+    
+   /*  c. output the average */
+    double average;  
+    double sum = 0;
+    for(cnt = 0; cnt < 10; cnt++)
+    {
+      sum = sum + MyArray[cnt];
+    }
+  
+    average = sum / 10;
+    
+    System.out.println("The average is: " + average);
+    
+   /* #2a). initialize array size 20 to all zeros, to (0,0,0,0,...), */
+    
+    
+   /*  b)  (1,2,3,1,2,3,1,2,3....)*/
+    
+    
+   /* #3 store the first 20 prime numbers into an array.*/
+
+    
+   /*  #4 Create an array with 100 Random numbers (between 1 and 200) */
+    
+    
+   /* a)What is the highest number, */
+  
+    
+   /*  b)Sum the odd values */
+  
+    
+   /* c) what is the most common value */
+  
+    
+    
+    
+    
+    
+    
+  }
+ 
+}
