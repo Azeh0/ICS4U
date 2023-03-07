110e6881d81c1b933b927e07ff41ea571e988e53
Anthony <95546821+Azeh0@users.noreply.github.com>
2023-03-06 20:39:31 -0500

    Binary Search in quiz review

---
+++ BinarySearch.java
@@ -0,0 +1,68 @@
+/**
+ * Auto Generated Java Class.
+ */
+public class BinarySearch {
+  
+  
+  public static void main(String[] args) { 
+    
+    Scanner MyInput = new Scanner(System.in);
+    int Inner, Outter, cnt, intMark = 0;
+    int[] intMyArray = new int[9];
+    long[] douMyArray = new long[10000];
+    long douTemp;
+    
+    for (cnt = 1; cnt <= 9999; cnt++)
+    {
+      douMyArray[cnt] = Math.round(Math.random() * 500);
+    }
+    
+    for (int i = 0; i < 10; i++)
+    {
+      System.out.println(douMyArray[i] + " ");
+    }
+    for (Outter = 0; Outter > 9998; Outter++)
+    {
+      for (Inner = Outter +1; Inner < 9999; Inner++)
+      {
+        if (douMyArray[Inner] < douMyArray[Outter])
+        {
+          douTemp = douMyArray[Inner];
+          douMyArray[Inner] = douMyArray[Outter];
+          douMyArray[Outter] = douTemp;
+        }
+      }
+    }
+System.out.print("Enter value to find: ");
+  int search = MyInput.nextInt();
+  
+  int first = 0;
+  int last = douMyArray.length - 1;
+  int middle = (first + last) / 2;
+  
+  while (first <= last)
+  {
+    if (duoMyArray[middle] == search)
+    {
+      System.out.println(search + " found at= " + (middle +1));
+      break;
+    }
+    else if (duoMyArray[middle] < search)
+    {
+      first = middle + 1;
+      System.out.println(middle);
+    }
+    else
+    {
+      last = middle -1;
+      System.out.println(middle);
+    }
+    middle = (first + last)/2;
+  }
+  if (first > last)
+    System.out.println(search + " Isn't present in the list. \n");
+ 
+    
+  }
+  
+}
--- QuizReview.java
+++ QuizReview.java
@@ -1,10 +1,12 @@
 import java.io.*;
-
+import java.util.Scanner;
 public class QuizReview {
   
   
   public static void main(String[] args)throws IOException  { 
     
+  Scanner MyInput = new Scanner(System.in); 
+  
   BufferedReader TheInptFl = new BufferedReader (new FileReader("Q2_NumbersA.txt"));
   
   BufferedWriter TheOtptFl = new BufferedWriter (new FileWriter("Result.txt"));
@@ -56,6 +58,61 @@ public class QuizReview {
   
   TheInptFl.close();
   TheOtptFl.close();
+  
+  for (int i = 0; i < 10; i++)                               
+  {
+    System.out.println(Data[i] + " ");
+  }
+  
+  for (int Outter = 0; Outter < Data.length ; Outter++)
+    {
+      for (int Inner = Outter +1; Inner < Data.length; Inner++ )
+        { 
+          if (Data[Inner] < Data[Outter])
+          {
+            int douTemp = Data[Inner];
+            Data[Inner] = Data[Outter];
+            Data[Outter] = douTemp;  
+          }
+         }
+     }
+ 
+     for (int k = 0; k < Data.length; k++)                               
+  {
+    System.out.println(Data[k] + " ");
+  }
+ 
+ 
+  System.out.println("Enter value to find");
+    int search = MyInput.nextInt();
+ 
+    int first  = 0;
+    int last   = Data.length - 1;
+    int middle = (first + last)/2;
+   
+   while( first <= last )
+   {
+     
+     if ( Data[middle] == search ) 
+      {
+       System.out.println(search + " found at location " + (middle + 1) + ".");
+       break;
+      }
+     else if (Data[middle] < search )
+      {
+       first = middle + 1;  
+       System.out.println(middle);
+      }
+     else
+       {
+        last = middle - 1;
+         System.out.println(middle);
+       }
+    middle = (first + last)/2;
+   }
+    if ( first > last )
+      System.out.println(search + " isn't present in the list.\n");
+    
   }
 }
 
